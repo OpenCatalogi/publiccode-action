bf580644b04b323ad23a22b5a00370e44d9285c0
Ruben van der Linde <ruben@conduction.nl>
2023-10-15 16:19:08 +0200

    Try the python code

--- update_publiccode.py
+++ update_publiccode.py
@@ -1,56 +1,57 @@
 # .github/scripts/update_publiccode.py
 
-import os
 import yaml
-import warnings
+import json
+from datetime import datetime
 
-# Check the metadata of the repository
-# For the simplicity, we're assuming that the metadata are environment variables
-# In a real case, you would fetch the metadata from the repository or other sources
-
-# Metadata variables
-variables = [
-    "GITHUB_REPOSITORY", 
-    "REPO_DESCRIPTION", 
-    "ORGANISATION", 
-    "TAGS", 
-    "WEBSITE", 
-    "LICENCE"
-]
-
-# Create a dictionary to hold the metadata
-metadata = {}
-
-# Check if each variable is set and if not, issue a warning
-for var in variables:
-    value = os.getenv(var)
-    if value:
-        metadata[var] = value
-    else:
-        warnings.warn(f"The {var} environment variable is not set")
-
-# Always set the repository url
-metadata["REPO_URL"] = f"https://github.com/{metadata.get('GITHUB_REPOSITORY', 'default_repo_name')}"
-
-# Read the current publiccode.yaml (if exists)
+# Read existing publiccode.yaml
 try:
-    with open("publiccode.yaml", "r") as file:
-        publiccode = yaml.safe_load(file)
+  with open("publiccode.yaml", "r") as f:
+      data = yaml.safe_load(f)
 except FileNotFoundError:
-    publiccode = {}
+  data = {}
 
-# Update the publiccode with the new metadata
-publiccode["publiccode"] = publiccode.get("publiccode", {})
-publiccode["publiccode"]["name"] = metadata.get("GITHUB_REPOSITORY")
-publiccode["publiccode"]["description"] = {
-    "nl": metadata.get("REPO_DESCRIPTION")
-}
-publiccode["publiccode"]["url"] = metadata.get("REPO_URL")
-publiccode["publiccode"]["tags"] = metadata.get("TAGS")
-publiccode["publiccode"]["website"] = metadata.get("WEBSITE")
-publiccode["publiccode"]["license"] = metadata.get("LICENCE")
-publiccode["publiccode"]["owner"] = metadata.get("ORGANISATION")
+# Convert created_at to date format
+# created_at_date = datetime.fromisoformat("$REPO_CREATED_AT".replace("Z", "+00:00")).strftime('%Y-%m-%d')
+created_at_date = datetime.now().strftime('%Y-%m-%d')
 
-# Write back the publiccode.yaml
-with open("publiccode.yaml", "w") as file:
-    yaml.safe_dump(publiccode, file)
+# Convert topics JSON string to Python list and then to comma-separated string
+
+# Update or append values
+if "$REPO_NAME" != "null" and "$REPO_NAME":
+  data['name'] = "$REPO_NAME"
+if "$REPO_URL" != "null" and "$REPO_URL":
+  data['url'] = "$REPO_URL"
+if "$REPO_DESC" != "null" and "$REPO_DESC":
+  data['description'] = "$REPO_DESC"
+if "$REPO_HOMEPAGE" != "null" and "$REPO_HOMEPAGE":
+  data['url'] = "$REPO_HOMEPAGE"
+#if "$REPO_TOPICS" != "null" and "$REPO_TOPICS":
+#    data['topics'] = "$REPO_TOPICS"
+if "$REPO_LICENSE" != "null" and "$REPO_LICENSE":
+  data['license'] = "$REPO_LICENSE"
+
+# Add releaseDate if not present
+if 'releaseDate' not in data:
+  data['releaseDate'] = created_at_date
+
+# Create or update nested 'organisation' array
+if 'organisation' not in data:
+  data['organisation'] = {}
+if "$ORGANISATION_NAME" != "null" and "$ORGANISATION_NAME":
+  data['organisation']['name'] = "$ORGANISATION_NAME"
+if "$ORGANISATION_AVATAR" != "null" and "$ORGANISATION_AVATAR":
+  data['organisation']['logo'] = "$ORGANISATION_AVATAR"
+if "$ORGANISATION_URL" != "null" and "$ORGANISATION_URL":
+  data['organisation']['url'] = "$ORGANISATION_URL"
+if "$ORGANISATION_DESCRIPTION" != "null" and "$ORGANISATION_DESCRIPTION":
+  data['organisation']['description'] = "$ORGANISATION_DESCRIPTION"
+
+# Create or update nested 'nl' array
+if 'nl' not in data:
+  data['nl'] = {}
+
+# Write updated publiccode.yaml
+with open("publiccode.yaml", "w") as f:
+  yaml.safe_dump(data, f)
+END
--- update_publicorganisation.py
+++ update_publicorganisation.py
@@ -1,56 +1,30 @@
-# .github/scripts/update_publiccode.py
+# .github/scripts/update_publicorganisation.py
 
-import os
 import yaml
-import warnings
+import json
+from datetime import datetime
 
-# Check the metadata of the repository
-# For the simplicity, we're assuming that the metadata are environment variables
-# In a real case, you would fetch the metadata from the repository or other sources
-
-# Metadata variables
-variables = [
-    "GITHUB_REPOSITORY", 
-    "REPO_DESCRIPTION", 
-    "ORGANISATION", 
-    "TAGS", 
-    "WEBSITE", 
-    "LICENCE"
-]
-
-# Create a dictionary to hold the metadata
-metadata = {}
-
-# Check if each variable is set and if not, issue a warning
-for var in variables:
-    value = os.getenv(var)
-    if value:
-        metadata[var] = value
-    else:
-        warnings.warn(f"The {var} environment variable is not set")
-
-# Always set the repository url
-metadata["REPO_URL"] = f"https://github.com/{metadata.get('GITHUB_REPOSITORY', 'default_repo_name')}"
-
-# Read the current publiccode.yaml (if exists)
+# Read existing openCatalogi.yaml
 try:
-    with open("publiccode.yaml", "r") as file:
-        publiccode = yaml.safe_load(file)
+  with open("openCatalogi.yaml", "r") as f:
+      data = yaml.safe_load(f)
 except FileNotFoundError:
-    publiccode = {}
+  data = {}
 
-# Update the publiccode with the new metadata
-publiccode["publiccode"] = publiccode.get("publiccode", {})
-publiccode["publiccode"]["name"] = metadata.get("GITHUB_REPOSITORY")
-publiccode["publiccode"]["description"] = {
-    "nl": metadata.get("REPO_DESCRIPTION")
-}
-publiccode["publiccode"]["url"] = metadata.get("REPO_URL")
-publiccode["publiccode"]["tags"] = metadata.get("TAGS")
-publiccode["publiccode"]["website"] = metadata.get("WEBSITE")
-publiccode["publiccode"]["license"] = metadata.get("LICENCE")
-publiccode["publiccode"]["owner"] = metadata.get("ORGANISATION")
+if "$ORGANISATION_NAME" != "null" and "$ORGANISATION_NAME":
+  data['name'] = "$ORGANISATION_NAME"
+if "$ORGANISATION_AVATAR" != "null" and "$ORGANISATION_AVATAR":
+  data['logo'] = "$ORGANISATION_AVATAR"
+if "$ORGANISATION_URL" != "null" and "$ORGANISATION_URL":
+  data['url'] = "$ORGANISATION_URL"
+if "$ORGANISATION_DESCRIPTION" != "null" and "$ORGANISATION_DESCRIPTION":
+  data['description'] = "$ORGANISATION_DESCRIPTION"
 
-# Write back the publiccode.yaml
-with open("publiccode.yaml", "w") as file:
-    yaml.safe_dump(publiccode, file)
+# Create or update nested 'nl' array
+if 'nl' not in data:
+  data['nl'] = {}
+
+# Write updated openCatalogi.yaml
+with open("openCatalogi.yaml", "w") as f:
+  yaml.safe_dump(data, f)
+END
